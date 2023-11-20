490916e774f81cdd0e633d34fa4ad6c701943649
William <robertw5@oregonstate.edu>
2023-11-20 14:09:40 -0500

    text field primary pg

--- src/main/java/artdealer/PrimaryController.java
+++ src/main/java/artdealer/PrimaryController.java
@@ -2,11 +2,14 @@ package artdealer;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
+        
+	// TextField nameTextField;
         App.setRoot("secondary");
     }
 }
