576b752772748409471efec6fb7fa250da289603
William <robertw5@oregonstate.edu>
2023-11-19 20:55:45 -0500

    employee mock data

--- target/classes/artdealer/SQL/DDL.sql
+++ target/classes/artdealer/SQL/DDL.sql
@@ -72,10 +72,10 @@ CREATE TABLE Employees
 -- ('Jimbo', 'Fisher', 'jfish@jfish.com', '265-783-6846'),
 -- ('Gibby', 'Loo', 'gibbloo@gloo.com', '583-376-8603');
 
--- INSERT INTO Employees (first_name, last_name, email, phone, title) 
--- VALUES ('Johnny', 'Appleseed', 'johnnyapp@zippity.com', '111-111-1111', 'The Man'),
--- ('Weeping', 'Willow', 'mopey@gmail.com', '222-222-2222', 'Not the Man'),
--- ('Ludwig', 'Van', 'thebest@besty.com', '123-456-7890', 'Worker');
+INSERT INTO Employees (first_name, last_name, email, phone, title) 
+VALUES ('Johnny', 'Appleseed', 'johnnyapp@zippity.com', '111-111-1111', 'The Man'),
+('Weeping', 'Willow', 'mopey@gmail.com', '222-222-2222', 'Not the Man'),
+('Ludwig', 'Van', 'thebest@besty.com', '123-456-7890', 'Worker');
 
 -- INSERT INTO Coffee (brand, coffee_name, roast_type, price, region, coffee_size) 
 -- VALUES ('Islandy', 'Island Chill', 'Medium', 14.99, 'Indonesia', 14),
