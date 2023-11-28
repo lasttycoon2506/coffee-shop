b2bd91df55724cb14b25df62a75444203f196fc4
William <robertw5@oregonstate.edu>
2023-11-28 15:47:44 -0500

    new customer pg entry field input testing

--- src/main/java/artdealer/NewCustomerController.java
+++ src/main/java/artdealer/NewCustomerController.java
@@ -1,9 +1,35 @@
 package artdealer;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class NewCustomerController {
+
+    @FXML
+	private Label myLabel;
+	@FXML
+	private TextField firstNameEntry;
+    @FXML
+	private TextField lastNameEntry;
+    @FXML
+	private TextField emailEntry;
+    @FXML
+	private TextField phoneEntry;
+	@FXML
+	private Button myButton;
+	
+	public void submit(ActionEvent event) {
+		
+		
+			System.out.println(firstNameEntry.getText());
+			
+			
+	}
 
     @FXML
     private void switchToHomePg() throws IOException {
@@ -13,5 +39,4 @@ public class NewCustomerController {
     void switchToCustomerPg() throws IOException {
         App.setRoot("customer");
     }
-    
 }
