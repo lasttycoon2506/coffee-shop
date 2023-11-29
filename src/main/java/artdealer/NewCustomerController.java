fcca4fb92ae318d19501d84d8c5ff300e32c9c6f
William <robertw5@oregonstate.edu>
2023-11-29 16:03:29 -0500

    input error checking

--- src/main/java/artdealer/NewCustomerController.java
+++ src/main/java/artdealer/NewCustomerController.java
@@ -2,6 +2,7 @@ package artdealer;
 
 import java.io.IOException;
 
+
 import artdealer.Models.CustomerDTO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -26,9 +27,36 @@ public class NewCustomerController {
 
 	public void submit(ActionEvent event) {
 		
+        if(firstNameEntry.getText().trim().isEmpty()){
+            firstNameEntry.setText("Please Enter First Name!");
+        }
+        if(lastNameEntry.getText().trim().isEmpty()){
+            lastNameEntry.setText("Please Enter Last Name!");
+        }
+        if(emailEntry.getText().trim().isEmpty()){
+            emailEntry.setText("Please Enter Email!");
+        }
+        if(phoneEntry.getText().trim().isEmpty()){
+            phoneEntry.setText("Please Enter Phone Number!");
+        }
+        else {
 		CustomerDTO newEntry = new CustomerDTO(firstNameEntry.getText(), lastNameEntry.getText(), emailEntry.getText(), phoneEntry.getText());
-			
+        }
 	}
+    public void resetEntryField(ActionEvent event){
+        if(firstNameEntry.getText() != null){
+            firstNameEntry.setText("");
+        }
+        if(lastNameEntry.getText().trim().isEmpty()){
+            lastNameEntry.setText("Please Enter Last Name!");
+        } 
+        if(emailEntry.getText().trim().isEmpty()){
+            emailEntry.setText("Please Enter Email!");
+        }
+        if(phoneEntry.getText().trim().isEmpty()){
+            phoneEntry.setText("Please Enter Phone Number!");
+        }
+    }
     @FXML
     private void switchToHomePg() throws IOException {
         App.setRoot("home");
