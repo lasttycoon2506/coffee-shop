c25fc39739e3483ed921b008d51fd34778f6abc8
William <robertw5@oregonstate.edu>
2023-11-29 16:24:50 -0500

    empty field validation

--- src/main/java/artdealer/NewCustomerController.java
+++ src/main/java/artdealer/NewCustomerController.java
@@ -1,14 +1,13 @@
 package artdealer;
 
 import java.io.IOException;
-
-
 import artdealer.Models.CustomerDTO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class NewCustomerController {
     @FXML
@@ -24,9 +23,8 @@ public class NewCustomerController {
 	@FXML
 	private Button myButton;
 	
-
-	public void submit(ActionEvent event) {
-		
+    @FXML
+	void submit(ActionEvent event) {
         if(firstNameEntry.getText().trim().isEmpty()){
             firstNameEntry.setText("Please Enter First Name!");
         }
@@ -43,24 +41,40 @@ public class NewCustomerController {
 		CustomerDTO newEntry = new CustomerDTO(firstNameEntry.getText(), lastNameEntry.getText(), emailEntry.getText(), phoneEntry.getText());
         }
 	}
-    public void resetEntryField(ActionEvent event){
+
+    @FXML
+    void resetFnameField(MouseEvent event) {
         if(firstNameEntry.getText() != null){
             firstNameEntry.setText("");
         }
-        if(lastNameEntry.getText().trim().isEmpty()){
-            lastNameEntry.setText("Please Enter Last Name!");
-        } 
-        if(emailEntry.getText().trim().isEmpty()){
-            emailEntry.setText("Please Enter Email!");
-        }
-        if(phoneEntry.getText().trim().isEmpty()){
-            phoneEntry.setText("Please Enter Phone Number!");
+    }
+
+    @FXML
+    void resetLnameField(MouseEvent event) {
+        if(lastNameEntry.getText() != null){
+            lastNameEntry.setText("");
         }
     }
+
+    @FXML
+    void resetEmailField(MouseEvent event) {
+        if(emailEntry.getText() != null){
+            emailEntry.setText("");
+        }
+    }
+
+    @FXML
+    void resetPhoneField(MouseEvent event) {
+        if(phoneEntry.getText() != null){
+            phoneEntry.setText("");
+        }
+    }
+
     @FXML
     private void switchToHomePg() throws IOException {
         App.setRoot("home");
     }
+
     @FXML
     void switchToCustomerPg() throws IOException {
         App.setRoot("customer");
