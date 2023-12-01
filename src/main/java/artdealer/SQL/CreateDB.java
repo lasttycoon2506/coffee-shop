68b59f5bc3e44a753ecefeb90cbce9e33c613333
William <robertw5@oregonstate.edu>
2023-12-01 16:28:32 -0500

    db pool jdbc

--- src/main/java/artdealer/SQL/CreateDB.java
+++ src/main/java/artdealer/SQL/CreateDB.java
@@ -78,6 +78,10 @@ public class CreateDB {
             e.printStackTrace();
         }
     }
+    
+    public void insertDB() {
+
+    }
 
     
 }
---
+++ src/main/java/artdealer/SQL/CreateSingleInstance.java
@@ -0,0 +1,14 @@
+package artdealer.SQL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateSingleInstance {
+   static final String DB_URL = "jdbc:mysql://localhost:3306/artshop";
+   static final String USER = "root";
+   static final String PASSWORD = "aggY836^@zT9&";
+    
+   //db pool to-do
+}
