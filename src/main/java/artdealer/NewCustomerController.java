00b84dd7cc49b630f2139f112ec9ba7ce42441a2
William <robertw5@oregonstate.edu>
2023-11-30 19:28:32 -0500

    more input val check

--- src/main/java/artdealer/NewCustomerController.java
+++ src/main/java/artdealer/NewCustomerController.java
@@ -63,6 +63,20 @@ public class NewCustomerController {
         fNameLabel.setText("");
     }
 
+    @FXML
+    void resetLnameLabel(KeyEvent event) {
+        lNameLabel.setText("");
+    }
+
+    @FXML
+    void resetEmailLabel(KeyEvent event) {
+        emailLabel.setText("");
+    }
+
+    @FXML
+    void resetPhoneLabel(KeyEvent event) {
+        phoneLabel.setText("");
+    }
 
     @FXML
     private void switchToHomePg() throws IOException {
