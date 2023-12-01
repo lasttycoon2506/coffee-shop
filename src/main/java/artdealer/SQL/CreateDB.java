9f9b9237477d78577561fffe1e5083dade7644bf
William <robertw5@oregonstate.edu>
2023-12-01 14:20:48 -0500

    create cust table sql db

--- src/main/java/artdealer/SQL/CreateDB.java
+++ src/main/java/artdealer/SQL/CreateDB.java
@@ -5,21 +5,34 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+// login to db...
+// mysql -uroot -p
 
 public class CreateDB {
-    static final String DB_URL = "jdbc:mysql://localhost:3306/";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/artshop";
     static final String USER = "root";
     static final String PASSWORD = "aggY836^@zT9&";
 
     public void connectDB() {
         try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
         Statement stmt = conn.createStatement();)
+
         {
-            System.out.println("Connected to DB..."); 
+            String createCustomerTableSQL = "CREATE OR REPLACE TABLE Customers " +
+                   "(customer_id INT AUTO_INCREMENT NOT NULL UNIQUE, " +
+                   " first_name VARCHAR(50) NOT NULL, " + 
+                   " last_name VARCHAR(50) NOT NULL, " + 
+                   " email VARCHAR(50) NOT NULL UNIQUE,  " + 
+                   " phone VARCHAR(50) NOT NULL UNIQUE, " +
+                   " PRIMARY KEY ( customer_id ))"; 
+        stmt.executeUpdate(createCustomerTableSQL);
+        System.out.println("Created table"); 
         }
         catch (SQLException e)
         {
             e.printStackTrace();
         }
     }
+
+    
 }
--- target/classes/artdealer/SQL/DDL.sql
+++ target/classes/artdealer/SQL/DDL.sql
@@ -6,7 +6,7 @@ SET sql_mode = 'STRICT_ALL_TABLES';
 CREATE TABLE Customers
 (
     customer_id INT NOT NULL AUTO_INCREMENT UNIQUE,
-    first_name VARCHAR(50)NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE,
     phone  VARCHAR(50) NOT NULL UNIQUE,
