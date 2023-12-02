9a48ad80203bc58536b3b6ca3cddff41e4841fde
William <robertw5@oregonstate.edu>
2023-12-02 13:25:28 -0500

    mvn clean pkg - removed hikaricp

--- src/main/java/artdealer/SQL/CreateSingleInstance.java
+++ src/main/java/artdealer/SQL/CreateSingleInstance.java
@@ -1,17 +1,8 @@
 package artdealer.SQL;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+
 
 public class CreateSingleInstance {
 
-   HikariConfig config = new HikariConfig();
-config.setJdbcUrl("jdbc:mysql://localhost:3306/simpsons");
-config.setUsername("bart");
-config.setPassword("51mp50n");0
-config.addDataSourceProperty("cachePrepStmts", "true");
-config.addDataSourceProperty("prepStmtCacheSize", "250");
-config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-
-HikariDataSource ds = new HikariDataSource(config);
+   
 }
