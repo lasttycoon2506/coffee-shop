5fdb1f06ffcc3844f592be20483dcdb71affefb9
William <robertw5@oregonstate.edu>
2023-12-02 16:34:45 -0500

    sql string interpolation

--- src/main/java/artdealer/SQL/InsertDB.java
+++ src/main/java/artdealer/SQL/InsertDB.java
@@ -9,15 +9,18 @@ public class InsertDB {
     public void InsertCustomerData(CustomerDTO customerdto) throws SQLException {
         Statement stmt = App.connection.createStatement();
 
-//         -- INSERT INTO Customers (first_name, last_name, email, phone) 
-// -- VALUES ('Chrissy', 'Rod', 'crod@crod.com', '263-643-4726'),
-// -- ('Jimbo', 'Fisher', 'jfish@jfish.com', '265-783-6846'),
-// -- ('Gibby', 'Loo', 'gibbloo@gloo.com', '583-376-8603');
-
 
         String insertCustomerSQL = "INSERT INTO Customers (first_name, last_name, email, phone)" +
-                            " VALUES () "; 
+                            " VALUES (@fname, @lname, @email, @phone ) "; 
                            
+                            SqlCommand.Parameters
+        using (var command = new SqlCommand(query, connection))
+        {
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@phone", phone);
+        
+            command.ExecuteNonQuery();
+                            }
                 stmt.executeUpdate(insertCustomerSQL);
     }
 }
