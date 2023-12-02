8208e36e4adb78f9922ed07cef09da630484e5d1
William <robertw5@oregonstate.edu>
2023-12-02 15:24:58 -0500

    returns sql conn obj

--- src/main/java/artdealer/SQL/CreateSingleInstance.java
+++ src/main/java/artdealer/SQL/CreateSingleInstance.java
@@ -3,15 +3,14 @@ package artdealer.SQL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class CreateSingleInstance {
     static final String DB_URL = "jdbc:mysql://localhost:3306/artshop";
     static final String USER = "root";
     static final String PASSWORD = "aggY836^@zT9&";
 
-    public static void ConnectDB() throws SQLException {
+    public static Connection ConnectDB() throws SQLException {
         Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-        Statement stmt = conn.createStatement();
+        return conn;
     }
 }
