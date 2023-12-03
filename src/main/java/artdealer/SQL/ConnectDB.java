13d75e0f2a54d584fcbba9e64775e76daefb2443
William <robertw5@oregonstate.edu>
2023-12-03 17:24:09 -0500

    more mvn clean install to get hikari recognized..

--- src/main/java/artdealer/SQL/ConnectDB.java
+++ src/main/java/artdealer/SQL/ConnectDB.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+// import com.zaxxer.hikari.HikariConfig;
+// import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectDB {
     static final String DB_URL = "jdbc:mysql://localhost:3306/artshop";
@@ -13,18 +13,18 @@ public class ConnectDB {
     static final String PASSWORD = "aggY836^@zT9&";
 
     public static Connection ConnectDb() throws SQLException {
-        // Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-        // return connection;
+        Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        return connection;
 
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/simpsons");
-        config.setUsername("bart");
-        config.setPassword("51mp50n");
-        config.addDataSourceProperty("cachePrepStmts", "true");
-        config.addDataSourceProperty("prepStmtCacheSize", "250");
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        // HikariConfig config = new HikariConfig();
+        // config.setJdbcUrl("jdbc:mysql://localhost:3306/simpsons");
+        // config.setUsername("bart");
+        // config.setPassword("51mp50n");
+        // config.addDataSourceProperty("cachePrepStmts", "true");
+        // config.addDataSourceProperty("prepStmtCacheSize", "250");
+        // config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
 
-        HikariDataSource ds = new HikariDataSource(config);
+        // HikariDataSource ds = new HikariDataSource(config);
     }
 
     
