fd923ebbd1eaaa4485493019e5dc2738deab7cf4
William <robertw5@oregonstate.edu>
2023-12-03 18:43:39 -0500

    insert cust data rebuild conn

--- src/main/java/artdealer/SQL/InsertDB.java
+++ src/main/java/artdealer/SQL/InsertDB.java
@@ -1,19 +1,19 @@
 package artdealer.SQL;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import artdealer.App;
 import artdealer.Models.CustomerDTO;
 public class InsertDB {
     
     public void InsertCustomerData(CustomerDTO customerdto) throws SQLException {
-        
-        Statement stmt = createStatement();
+        Connection connection = ConnectDB.ConnectDb();
+        Statement stmt = connection.createStatement();
 
         String insertCustomerSQL = "INSERT INTO Customers (first_name, last_name, email, phone)" +
                             "VALUES ('" +customerdto.firstName()+ "', '" +customerdto.lastName()+ 
                             "', '" +customerdto.email()+ "', '" +customerdto.phone()+ "')";   
         
-        // stmt.executeUpdate(insertCustomerSQL);
+        stmt.executeUpdate(insertCustomerSQL);
     }
 }
