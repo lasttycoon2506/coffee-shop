3fec6d15ae782422bf8586afa43e41652e770923
William <robertw5@oregonstate.edu>
2023-12-05 14:45:20 -0500

    insert cust w/ user/pw

--- src/main/java/artdealer/SQL/InsertDB.java
+++ src/main/java/artdealer/SQL/InsertDB.java
@@ -7,14 +7,14 @@ import artdealer.Models.CustomerDTO;
 
 
 public class InsertDB {
-    
     public void InsertCustomerData(CustomerDTO customerdto) throws SQLException {
         Connection connection = ConnectDB.ConnectDb();
         Statement stmt = connection.createStatement();
 
-        String insertCustomerSQL = "INSERT INTO Customers (first_name, last_name, email, phone)" +
+        String insertCustomerSQL = "INSERT INTO Customers (user_name, pword, first_name, last_name, email, phone)" +
                             "VALUES ('" +customerdto.firstName()+ "', '" +customerdto.lastName()+ 
-                            "', '" +customerdto.email()+ "', '" +customerdto.phone()+ "')";   
+                                    "', '" +customerdto.email()+ "', '" +customerdto.phone()+
+                                    "', '" +customerdto.email()+ "', '" +customerdto.phone()+ "')";   
         
         stmt.executeUpdate(insertCustomerSQL);
     }
