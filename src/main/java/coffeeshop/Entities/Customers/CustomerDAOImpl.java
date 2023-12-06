cda0b02e2b956c45400b2777134cc7bbd7f4763e
William <robertw5@oregonstate.edu>
2023-12-06 18:48:48 -0500

    hibernate session impl

--- src/main/java/coffeeshop/Entities/Customers/CustomerDAOImpl.java
+++ src/main/java/coffeeshop/Entities/Customers/CustomerDAOImpl.java
@@ -2,9 +2,16 @@ package coffeeshop.Entities.Customers;
 
 import java.util.List;
 import org.hibernate.Session;
-
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CustomerDAOImpl implements CustomerDAO {
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     public void save(Customer customer){}
 
     public List<Customer> listView(){
