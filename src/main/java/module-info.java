7ddc3b61be459cea9ba6fa6a78104fb86dc36385
William <robertw5@oregonstate.edu>
2023-12-06 18:46:43 -0500

    hibernate module-info.jav

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,9 @@ module coffeeshop {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.hibernate.orm.core;
 
-    opens coffeeshop to javafx.fxml;
+    opens coffeeshop to javafx.fxml, org.hibernate.orm.core;
     exports coffeeshop;
     opens coffeeshop.Controllers to javafx.fxml;
     exports coffeeshop.Controllers;
