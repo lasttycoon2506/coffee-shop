f6c36ede32da77ad41ed799b24c7130c2e202f01
William <robertw5@oregonstate.edu>
2023-12-07 14:16:40 -0500

    cust dao implemented

--- src/main/java/coffeeshop/Entities/Customers/CustomerDAO.java
+++ src/main/java/coffeeshop/Entities/Customers/CustomerDAO.java
@@ -1,16 +1,55 @@
 package coffeeshop.Entities.Customers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import coffeeshop.Models.DAO;
 
-public interface CustomerDAO {
+public class CustomerDAO implements DAO<Customer> {
+    private SessionFactory sessionFactory;
 
-    public void save(Customer customer);
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
-    public List<Customer> listView();
+    public void get(Customer customer) {
 
-    public void update(Customer customer);
+    }
 
-    public void delete(Customer customer);
+    public List<Customer> getAll(){
+        return null;
+    }
+
+    public void save(Customer customer){
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(customer);
+        tx.commit();
+        session.close();
+    }
+
+    public List<Customer> listView(){
+        return null;
+    }
+
+    public void update(Customer customer){
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(customer);
+        tx.commit();
+        session.close();
+    }
+
+    public void delete(Customer customer){}
+
+    @Override
+    public Optional<Customer> get(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
 
 }
--- src/main/java/coffeeshop/Models/DAO.java
+++ src/main/java/coffeeshop/Models/DAO.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface DAO<Type> {
-    Optional<Type> get(long id);
+    Optional<Type> get(int id);
     List<Type> getAll();
-    public void save(Type t);
-    void update(Type t, String[] params);
+    void save(Type t);
+    void update(Type t);
     void delete(Type t);
 }
