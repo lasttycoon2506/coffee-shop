0d81514129ee1e2556e61d15a7e54b9dd9ed7ac1
William <robertw5@oregonstate.edu>
2023-12-08 13:16:11 -0500

    db pw change conflict w persistxml file

--- src/main/java/coffeeshop/DB/ConnectDB.java
+++ src/main/java/coffeeshop/DB/ConnectDB.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 public class ConnectDB {
     static final String DB_URL = "jdbc:mysql://localhost:3306/coffeeshop";
     static final String USER = "root";
-    static final String PASSWORD = "aggY836^@zT9&";
+    static final String PASSWORD = "aggY836^@zT9";
 
     public static Connection ConnectDb() throws SQLException {
         Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
