2641b97bdfd99785460444bf27110a9daac53bbb
William <robertw5@oregonstate.edu>
2023-12-08 16:32:31 -0500

    cust jpa fields unique

--- src/main/java/coffeeshop/Entities/Customers/Customer.java
+++ src/main/java/coffeeshop/Entities/Customers/Customer.java
@@ -1,10 +1,12 @@
 package coffeeshop.Entities.Customers;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name = "Customers")
@@ -12,16 +14,21 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer customer_id;
+
+    @Column(unique = true)
     private String user_name;
+
     private String pword;
+
     private String first_name;
+
     private String last_name;
+
+    @Column(unique = true)
     private String email;
+    
+    @Column(unique = true)
     private String phone;
-
-    public Customer() {
-
-    }
     
     public Customer (String user_name, String pword, String first_name, String last_name, String email, String phone){
         this.user_name = user_name;
