46d0b99ec960a272f1e66d14636d8b4783a9ba34
William <robertw5@oregonstate.edu>
2023-12-12 10:40:19 -0500

    bcrypt to pom

--- src/main/java/coffeeshop/Security/Login.java
+++ src/main/java/coffeeshop/Security/Login.java
@@ -3,7 +3,12 @@ package coffeeshop.Security;
 
 public class Login {
     String  originalPassword = "password";
+		// String generatedSecuredPasswordHash = BCrypt.hashpw(originalPassword, BCrypt.gensalt(12));
+		// System.out.println(generatedSecuredPasswordHash);
 
-    // PBKDF2WithHmacSHA1 algo...
+		// boolean matched = BCrypt.checkpw(originalPassword, generatedSecuredPasswordHash);
+		// System.out.println(matched);
 
 }
+
+
