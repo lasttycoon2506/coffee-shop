85f75494fcc56626e5786b002e098d5aa9099579
William <robertw5@oregonstate.edu>
2023-12-16 16:03:05 -0500

    cleanup

--- src/main/java/coffeeshop/Controllers/Customers/CustomerProfile.java
+++ src/main/java/coffeeshop/Controllers/Customers/CustomerProfile.java
@@ -3,23 +3,14 @@ package coffeeshop.Controllers.Customers;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
-
 import coffeeshop.App;
 import coffeeshop.Models.Context;
 import coffeeshop.Entities.Customers.Customer;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.util.converter.IntegerStringConverter;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextFormatter;
 import javafx.scene.control.TableColumn;
 
 
@@ -48,24 +39,20 @@ public class CustomerProfile {
 		data = FXCollections.observableArrayList( new Customer(customer.getUserName(), customer.getPassword(), customer.getFirstName(),
 														customer.getLastName(), customer.getEmail(), customer.getPhone())
         );
+		createTable();
     }
 	
 
 	public void createTable()  {
-        userColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-        pwColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        firstNColumn
-.setCellValueFactory(new PropertyValueFactory<>("birth"));
-        lastNColumn.setCellValueFactory(new PropertyValueFactory<>("adress"));
-        emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
-		phoneColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
-
-		// table.setItems(null);
-		
-		
-
-		
+		table.setEditable(true);
+ 
+        userColumn.setCellValueFactory(new PropertyValueFactory<Customer, String>("user_name"));
+        pwColumn.setCellValueFactory(new PropertyValueFactory<Customer, String>("passWord"));
+        firstNColumn.setCellValueFactory(new PropertyValueFactory<Customer, String>("firstName"));
+ 
+        table.setItems(data);
 	}
+	
     @FXML
     private void switchToCustomerPg() throws IOException {
         App.setRoot("customer");
