e07ee376934fd60d1d275a2cbdd6ed20ddf312f3
William <robertw5@oregonstate.edu>
2023-12-16 10:17:51 -0500

    customer prof pg table

--- src/main/java/coffeeshop/Controllers/Customers/CustomerProfile.java
+++ src/main/java/coffeeshop/Controllers/Customers/CustomerProfile.java
@@ -12,64 +12,81 @@ import javafx.scene.control.TableColumn;
 
 
 public class CustomerProfile {
+	@FXML
+    private TableView<Customer> customerTable;
+	@FXML
+	private TableColumn<Customer, String> userCol;
+	@FXML
+	private TableColumn<Customer, String> pwCol;
+	@FXML
+	private TableColumn<Customer, String> firstNCol;
+	@FXML
+	private TableColumn<Customer, String> lastNCol;
+	@FXML
+	private TableColumn<Customer, String> emailCol;
+	@FXML
+	private TableColumn<Customer, String> phoneCol;
 
-    TableView<Person> table = new TableView<Person>();
-		table.setEditable(true);
+
+	public void createTable() {
+		customerTable.setEditable(true);
 				
-		TableColumn<Person, String> firstNameColumn = new TableColumn<Person, String>("First Name");
-		firstNameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("firstName"));
-		firstNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-		firstNameColumn.setOnEditCommit(new EventHandler<CellEditEvent<Person, String>>() {
-			@Override
-			public void handle(CellEditEvent<Person, String> event) {
-				Person person = event.getRowValue();
-				person.setFirstName(event.getNewValue());
-			}
-		});
-
-
-		TableColumn<Person, String> lastNameColumn = new TableColumn<Person, String>("Last Name");
-		lastNameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
-		lastNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-		lastNameColumn.setOnEditCommit(new EventHandler<CellEditEvent<Person, String>>() {
-			@Override
-			public void handle(CellEditEvent<Person, String> event) {
-				Person person = event.getRowValue();
-				person.setLastName(event.getNewValue());
-			}
-		});
-
-		TableColumn<Person, Integer> ageColumn = new TableColumn<Person, Integer>("Age");
-		ageColumn.setCellValueFactory(new PropertyValueFactory<Person, Integer>("age"));
-		ageColumn.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
-		ageColumn.setOnEditCommit(new EventHandler<CellEditEvent<Person, Integer>>() {
-			@Override
-			public void handle(CellEditEvent<Person, Integer> event) {
-				Person person = event.getRowValue();
-				person.setAge(event.getNewValue());
-			}
-		});
-		
-		table.getColumns().add(firstNameColumn);
-		table.getColumns().add(lastNameColumn);
-		table.getColumns().add(ageColumn);
-		
-		table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-		
-		table.getItems().add(new Person("Buggs", "Bunny", 79));
-		table.getItems().add(new Person("Daffy", "Duck", 83));
-		table.getItems().add(new Person("Foghorn", "Leghorn", 74));
-		table.getItems().add(new Person("Elmer", "Fudd", 83));
-		table.getItems().add(new Person("Tweety", "Bird", 73));
-		
-		root.setCenter(table);
-
-		Scene scene = new Scene(root, 500, 300);
-		scene.getStylesheets().add("application/stylesheet.css");
-		primaryStage.setTitle("TableView Demo");
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		userCol.setCellValueFactory(new PropertyValueFactory<Customer, String>("firstName"));
+		userCol.setCellFactory(TextFieldTableCell.forTableColumn());
 	}
+	// 	firstNameColumn.setOnEditCommit(new EventHandler<CellEditEvent<Customer, String>>() {
+	// 		@Override
+	// 		public void handle(CellEditEvent<Person, String> event) {
+	// 			Customer customer = event.getRowValue();
+	// 			customer.setFirstName(event.getNewValue());
+	// 		}
+	// 	});
+	// }
+		
+
+
+	// 	TableColumn<Person, String> lastNameColumn = new TableColumn<Person, String>("Last Name");
+	// 	lastNameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
+	// 	lastNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+	// 	lastNameColumn.setOnEditCommit(new EventHandler<CellEditEvent<Person, String>>() {
+	// 		@Override
+	// 		public void handle(CellEditEvent<Person, String> event) {
+	// 			Person person = event.getRowValue();
+	// 			person.setLastName(event.getNewValue());
+	// 		}
+	// 	});
+
+	// 	TableColumn<Person, Integer> ageColumn = new TableColumn<Person, Integer>("Age");
+	// 	ageColumn.setCellValueFactory(new PropertyValueFactory<Person, Integer>("age"));
+	// 	ageColumn.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+	// 	ageColumn.setOnEditCommit(new EventHandler<CellEditEvent<Person, Integer>>() {
+	// 		@Override
+	// 		public void handle(CellEditEvent<Person, Integer> event) {
+	// 			Person person = event.getRowValue();
+	// 			person.setAge(event.getNewValue());
+	// 		}
+	// 	});
+		
+	// 	table.getColumns().add(firstNameColumn);
+	// 	table.getColumns().add(lastNameColumn);
+	// 	table.getColumns().add(ageColumn);
+		
+	// 	table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+		
+	// 	table.getItems().add(new Person("Buggs", "Bunny", 79));
+	// 	table.getItems().add(new Person("Daffy", "Duck", 83));
+	// 	table.getItems().add(new Person("Foghorn", "Leghorn", 74));
+	// 	table.getItems().add(new Person("Elmer", "Fudd", 83));
+	// 	table.getItems().add(new Person("Tweety", "Bird", 73));
+		
+	// 	root.setCenter(table);
+
+	// 	Scene scene = new Scene(root, 500, 300);
+	// 	scene.getStylesheets().add("application/stylesheet.css");
+	// 	primaryStage.setTitle("TableView Demo");
+	// 	primaryStage.setScene(scene);
+	// 	primaryStage.show();
+	// }
 
     @FXML
     private void switchToCustomerPg() throws IOException {
@@ -79,4 +96,6 @@ public class CustomerProfile {
     private void switchToHomePg() throws IOException {
         App.setRoot("home");
     }
+
 }
+
