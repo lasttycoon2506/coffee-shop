06f7f4f68e966866cf0f8b2e0ad0323208f08659
William <robertw5@oregonstate.edu>
2023-12-18 19:18:53 -0500

    getters coffee class

--- src/main/java/coffeeshop/Entities/Coffee/Coffee.java
+++ src/main/java/coffeeshop/Entities/Coffee/Coffee.java
@@ -21,8 +21,8 @@ public class Coffee {
     private Integer coffee_size;
 
     public Coffee(){
-
     }
+
     public Coffee(String brand, String coffee_name, String roast_type, Float price, String region, Integer coffee_size){
         this.brand = brand;
         this.coffee_name = coffee_name;
@@ -30,5 +30,24 @@ public class Coffee {
         this.price = price;
         this.region = region;
         this.coffee_size = coffee_size;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+    public String getCoffeeName(){
+        return coffee_name;
+    }
+    public String getRoastType(){
+        return roast_type;
+    }
+    public Float getPrice(){
+        return price;
+    }
+    public String getRegion(){
+        return region;
+    }
+    public Integer getCoffeeSize(){
+        return coffee_size;
     }
 }
