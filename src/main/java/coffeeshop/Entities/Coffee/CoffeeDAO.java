0007af28a1bfbc7fecd9da25387aa7fb6abc0348
William <robertw5@oregonstate.edu>
2023-12-21 15:29:21 -0500

    CoffeeDAOService

--- src/main/java/coffeeshop/Entities/Coffee/CoffeeDAO.java
+++ src/main/java/coffeeshop/Entities/Coffee/CoffeeDAO.java
@@ -6,10 +6,12 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+
 public class CoffeeDAO {
     private static final EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa-hibernate-mysql");
     private static final EntityManager entityManager = factory.createEntityManager();
-    public void save(Coffee coffee) {
+
+    public static void save(Coffee coffee) {
         executeInsideTransaction(entityManager -> entityManager.persist(coffee));
     }
 
--- src/main/java/coffeeshop/Entities/Coffee/CoffeeDAOService.java
+++ src/main/java/coffeeshop/Entities/Coffee/CoffeeDAOService.java
@@ -1,5 +1,9 @@
 package coffeeshop.Entities.Coffee;
 
+
 public class CoffeeDAOService {
     
+    public static void saveCoffee(Coffee coffee) {
+        CoffeeDAO.save(coffee);
+    }
 }
