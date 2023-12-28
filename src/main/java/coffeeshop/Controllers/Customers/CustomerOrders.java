cc77a7168022846a4b8ceff5a8a3505a2d6f84a1
William <robertw5@oregonstate.edu>
2023-12-28 15:21:05 -0500

    orders table

--- src/main/java/coffeeshop/Controllers/Customers/CustomerOrders.java
+++ src/main/java/coffeeshop/Controllers/Customers/CustomerOrders.java
@@ -1,11 +1,22 @@
 package coffeeshop.Controllers.Customers;
 
 import java.io.IOException;
-
+import java.time.LocalDate;
 import coffeeshop.App;
+import coffeeshop.Entities.Orders.Order;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 
 public class CustomerOrders {
+    private TableView<Order> ordersTable;
+    @FXML
+	private TableColumn<Order, LocalDate> orderDateColumn;
+    @FXML
+	private TableColumn<Order, Integer> totalItemsColumn;
+
+	
     @FXML
     private void switchToCustomerPg() throws IOException {
         App.setRoot("customer");
