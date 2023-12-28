b80e973ddcb047b853871a51512b9c02caec627a
William <robertw5@oregonstate.edu>
2023-12-28 18:34:25 -0500

    removed region/name coffee entity

--- src/main/java/coffeeshop/Entities/Coffee/Coffee.java
+++ src/main/java/coffeeshop/Entities/Coffee/Coffee.java
@@ -16,39 +16,28 @@ public class Coffee {
     @Column(name = "coffee_id")
     private int coffeeId;
     private String brand;
-    @Column(name = "coffee_name")
-    private String coffeeName;
     private String roast;
     private String price;
-    private String region;
     @Column(name = "coffee_size")
     private int coffeeSize;
 
     public Coffee(){
     }
-    public Coffee (String brand, String coffeeName, String roast, String price, String region, int coffeeSize) {
+    public Coffee (String brand, String roast, String price, int coffeeSize) {
         this.brand = brand;
-        this.coffeeName = coffeeName;
         this.roast = roast;
         this.price = price;
-        this.region = region;
         this.coffeeSize = coffeeSize;
     }
 
     public String getBrand(){
         return brand;
     }
-    public String getCoffeeName(){
-        return coffeeName;
-    }
     public String getRoast(){
         return roast;
     }
     public String getPrice(){
         return price;
-    }
-    public String getRegion(){
-        return region;
     }
     public int getCoffeeSize(){
         return coffeeSize;
